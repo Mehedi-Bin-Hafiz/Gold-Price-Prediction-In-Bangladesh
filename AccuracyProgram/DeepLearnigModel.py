5fbaed153720c4aadc1c7c67cbda4f01356b4070
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-11-17 10:14:21 +0600

    motamuti complete

--- AccuracyProgram/DeepLearnigModel.py
+++ AccuracyProgram/DeepLearnigModel.py
@@ -1,84 +1,72 @@
 
 import pandas as pd
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, LSTM, Dropout
 from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
+from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.pipeline import Pipeline
+import numpy as np
+import matplotlib.pyplot as plt
 
 #database
-MainDatabase = pd.read_excel("../Database/MainDataset.xlsx")
+TrainingDataSet = pd.read_excel("../Database/MainDataset.xlsx").iloc[:200]
 # base on database we will set iloc
-X = MainDatabase.iloc[:, 0:5].values  #independent variables
-X=X.astype(int)
-print(X)
-Y = MainDatabase.iloc[ : , -1].values #dependent variables
-print(Y)
+"""Scaler"""
+scaler=MinMaxScaler()
+TrainingDataSet=scaler.fit_transform(TrainingDataSet)
+print(TrainingDataSet.shape)
+
+x_train,y_train=[],[]
+for i in range(60,TrainingDataSet.shape[0]):
+    x_train.append(TrainingDataSet[i-60:i])
+    y_train.append(TrainingDataSet[i,6])
+
+x_train,y_train=np.array(x_train),np.array(y_train)
+x_test,y_test=list(),list()
+TestingDataSet = pd.read_excel("../Database/MainDataset.xlsx").iloc[:200]
+
+TestingDataSet=scaler.fit_transform(TestingDataSet)
+
+for j in range(60,TestingDataSet.shape[0]):
+    x_test.append(TestingDataSet[j-60:j])
+    y_test.append(TestingDataSet[j,6])
+
+x_test,y_test=np.array(x_test),np.array(y_test)
 
 
+regressor= Sequential()
 
+regressor.add(LSTM(units= 100,activation="relu", return_sequences=True, input_shape=(x_train.shape[1],7)))
+regressor.add(Dropout(0.2))
 
-# # define base model
-# def baseline_model():
-# 	# create model
-# 	model = Sequential()
-# 	model.add(Dense(32, input_dim=5, kernel_initializer='normal', activation='relu'))
-# 	model.add(Dense(1, kernel_initializer='normal'))
-# 	# Compile model
-# 	model.compile(loss='mean_squared_error', optimizer='adam')
-# 	return model
-# # evaluate model
-# estimator = KerasRegressor(build_fn=baseline_model, epochs=100, batch_size=5, verbose=0)
-# kfold = KFold(n_splits=10)
-# results = cross_val_score(estimator, X, Y, cv=kfold)
-# print("Baseline: %.2f (%.2f) MSE" % (results.mean(), results.std()))
-#
+regressor.add(LSTM(units= 120,activation="relu", return_sequences=True))
+regressor.add(Dropout(0.30))
 
+regressor.add(LSTM(units= 160,activation="relu", return_sequences=True))
+regressor.add(Dropout(0.40))
+regressor.add(LSTM(units= 200,activation="relu", return_sequences=False)) #this false is vvi
+regressor.add(Dropout(0.50))
+regressor.add(Dense(units=1))
 
+regressor.summary()
 
-###Sequestial model
-
-import matplotlib.pyplot as plt
-import numpy as np
-np.random.seed(0)
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation
-
-
-x_train,y_train,x_test,y_test=X[:2100],Y[:2100],X[2100:],Y[2100:]
-model = Sequential(
-    [
-        Dense(256,),
-        Activation('relu'),
-        Dropout(0.5),
-        Dense(128, ),
-        Activation('relu'),
-        Dropout(0.5),
-        Dense(64, ),
-        Activation('relu'),
-        Dropout(0.5),
-        Activation('softmax')
-    ]
-)
+es = EarlyStopping(monitor='val_loss', patience = 3, verbose=1, mode='min')
+regressor.compile(optimizer='adam', loss='mean_squared_error',metrics=['accuracy'])
+history=regressor.fit(x_train,y_train,epochs=10,batch_size=32, validation_split = 0.1,callbacks=[es])
 
 
 
 
 
-model.summary()
-
-model.compile(optimizer='adam',loss= 'categorical_crossentropy',
-              metrics=['accuracy'])
-
-history = model.fit(x_train,y_train,epochs=100,batch_size=32,validation_split = 0.1, )
 
 
 
 """ Evaluate Model on Test data"""
 
-model.evaluate(x_test,y_test,batch_size=32, verbose=1)
+regressor.evaluate(x_test,y_test,batch_size=32, verbose=1)
 plt.plot(history.history['loss'],label='Trainig Loss')
 plt.plot(history.history['val_loss'],label="validation loss")
 plt.title("Training and validation loss")
@@ -88,7 +76,7 @@ plt.legend()
 plt.show()
 
 """Accuracy"""
-model.evaluate(x_test,y_test,batch_size=32, verbose=1)
+regressor.evaluate(x_test,y_test,batch_size=32, verbose=1)
 plt.plot(history.history['accuracy'],label='Trainig Accuracy')
 plt.plot(history.history['val_accuracy'],label="validation Accuracy")
 plt.title("Training and validation Accuracy")
